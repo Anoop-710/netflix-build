483a06fbb0389ae37edd2c78e362df8a1233315d
anoop <anoopaathreya@gmail.com>
2022-12-11 09:29:38 +0530

    firebase authentication completed

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDRocgB-UKO7msARUm8PDoHDnP2OvHVJ3E",
+    authDomain: "netflix-build-fc6ee.firebaseapp.com",
+    projectId: "netflix-build-fc6ee",
+    storageBucket: "netflix-build-fc6ee.appspot.com",
+    messagingSenderId: "273447270596",
+    appId: "1:273447270596:web:34afb49ce6b7fe023f3191"
+  };
